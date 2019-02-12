c29553e0f63943f9145ee3a1b12024e567cbbcef
Jan Aagaard <jan@aagaard.net>
2019-02-12 13:58:56 +0100

    Return objects

--- src/greet/index.ts
+++ src/greet/index.ts
@@ -1,12 +1,12 @@
 export async function run(context: any, request: any) {
   if (request.query.name || (request.body && request.body.name)) {
-    context.res = {
+    return {
       // status: 200, /* Defaults to 200 */
       body: "Hello " + (request.query.name || request.body.name)
     };
   }
   else {
-    context.res = {
+    return {
       status: 400,
       body: "Please pass a name on the query string or in the request body."
     };
