e233c91bf7ddad81152638339a5189fd0841adac
Jan Aagaard <jan@aagaard.net>
2019-02-12 11:49:22 +0100

    Add VS Code setup

--- greet/index.js
+++ greet/index.js
@@ -1,6 +1,4 @@
 module.exports = async function (context, req) {
-  context.log('JavaScript HTTP trigger function processed a request.');
-
   if (req.query.name || (req.body && req.body.name)) {
     context.res = {
       // status: 200, /* Defaults to 200 */
