b13317cee6c0f920408960a68cb8e694e69572de
Jan Aagaard <jan@aagaard.net>
2019-02-12 14:04:22 +0100

    Invert if

--- src/greet/index.ts
+++ src/greet/index.ts
@@ -1,17 +1,16 @@
 export async function run(context: any, request: any) {
   const name = getName(request);
-  if (name) {
-    return {
-      // status: 200, /* Defaults to 200 */
-      body: "Hello " + name
-    };
-  }
-  else {
+  if (!name) {
     return {
       status: 400,
       body: "Please pass a name on the query string or in the request body."
     };
   }
+
+  return {
+    // status: 200, /* Defaults to 200 */
+    body: "Hello " + name
+  };
 };
 
 function getName(request: any): string {
