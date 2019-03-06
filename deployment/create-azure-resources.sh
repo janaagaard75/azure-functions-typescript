d817397530a7914908c428924e22e9c2b66b1819
Jan Aagaard <jan@aagaard.net>
2019-03-06 16:52:03 +0100

    Wrap in back quotes

--- deployment/create-azure-resources.sh
+++ deployment/create-azure-resources.sh
@@ -19,7 +19,7 @@ az group create \
 # Create the resources.
 script_folder="$( cd "$(dirname ${BASH_SOURCE[0]})"; pwd -P )"
 template_file=$script_folder/azure-resources-template.json
-now=date +"%Y%m%d-%H%M%S"
+now=`date +"%Y%m%d-%H%M%S"`
 deployment_name="CircleCI-$now"
 echo "Creating resources in resource group $resource_group_name with deployment $deployment_name."
 az group deployment create \
