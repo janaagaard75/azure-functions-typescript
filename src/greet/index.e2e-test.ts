a74f1a05054c756e688d81b4920af40fdfd4321c
Jan Aagaard <jan@aagaard.net>
2019-03-15 15:30:21 +0100

    Add POST test

--- src/greet/index.e2e-test.ts
+++ src/greet/index.e2e-test.ts
@@ -1,12 +1,23 @@
+import * as fs from "fs";
 import fetch from "node-fetch";
 import { TestHelper } from "../TestHelper";
 
 describe("greet endpoint", () => {
-  test("returns correct greeting", async () => {
+  test("returns correct greeting when using GET", async () => {
     const response = await fetch(
       `${TestHelper.apiRootUrl}/greet?name=Jan+Aagaard`
     );
     const body = await response.text();
     expect(body).toBe("Hello Jan Aagaard");
+  });
+
+  test("return correct greeting when using POST", async () => {
+    const requestBody = fs.readFileSync("src/greet/sample.dat").toString();
+    const response = await fetch(`${TestHelper.apiRootUrl}/greet`, {
+      body: requestBody,
+      method: "POST"
+    });
+    const responseBody = await response.text();
+    expect(responseBody).toBe("Hello Jan Aagaard");
   });
 });
