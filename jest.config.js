36ec02195db517764915f4240692ef74d203e555
Jan Aagaard <jan@aagaard.net>
2019-03-20 23:37:54 +0100

    Increase timeout to 30 seconds

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   roots: ["<rootDir>/src"],
+  setupFilesAfterEnv: ["./jest.setup.js"],
   testEnvironment: "node",
   testRegex: "\\.(test|e2e-test)\\.ts$"
 };
---
+++ jest.setup.js
@@ -0,0 +1,2 @@
+// Increase timeout to 30 seconds to allow the Azure functions to spin up.
+jest.setTimeout(30 * 1000);
