1fe2a566c3c3639af3716cb5406da13931562aa8
Jan Aagaard <jan@aagaard.net>
2019-07-16 22:51:48 +0200

    Add full stop

--- src/greet/greet.ts
+++ src/greet/greet.ts
@@ -8,7 +8,7 @@ export async function greet(
   const name = extractName(request);
   if (name === undefined) {
     return {
-      body: "Please pass a name on the query string or in the request body",
+      body: "Please pass a name on the query string or in the request body.",
       status: 400
     };
   }
