6ff0a0cdde10a2d057aa46bcecaceea8af4259af
Jan Aagaard <jan@aagaard.net>
2019-07-16 23:07:03 +0200

    Test both query and body

--- src/greet/greet.test.ts
+++ src/greet/greet.test.ts
@@ -9,10 +9,23 @@ describe.each([
   [null, "Please pass a name on the query string or in the request body."],
   [" ", "Please pass a name on the query string or in the request body."]
 ])("greet function", (name, greeting) => {
-  test(`returns correct greeting for '${name}'`, async () => {
+  test(`returns correct greeting for '${name}' in the query`, async () => {
     const request = Substitute.for<HttpRequest>();
     (request.query.returns as any)({ name });
     (request.body.returns as any)({});
+
+    const context = Substitute.for<Context>();
+    (context.req as any).returns({ req: request });
+
+    const response = await greet(context, request);
+
+    expect(response.body).toBe(greeting);
+  });
+
+  test(`returns correct greeting for '${name}' in the body`, async () => {
+    const request = Substitute.for<HttpRequest>();
+    (request.query.returns as any)({});
+    (request.body.returns as any)({ name });
 
     const context = Substitute.for<Context>();
     (context.req as any).returns({ req: request });
