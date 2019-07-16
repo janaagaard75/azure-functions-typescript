19313018cd79a7e04132622196a1db640cd2d01f
Jan Aagaard <jan@aagaard.net>
2019-07-16 22:54:49 +0200

    Also test for missing name

--- src/greet/greet.test.ts
+++ src/greet/greet.test.ts
@@ -3,20 +3,22 @@ import { HttpRequest } from "@azure/functions";
 import { Substitute } from "@fluffy-spoon/substitute";
 import { greet } from "./greet";
 
-describe("greet function", () => {
-  test("returns correct greeting", async () => {
+describe.each([
+  ["Jan Aagaard", "Hello Jan Aagaard."],
+  ["", "Please pass a name on the query string or in the request body."],
+  [null, "Please pass a name on the query string or in the request body."],
+  [" ", "Please pass a name on the query string or in the request body."]
+])("greet function", (name, greeting) => {
+  test(`returns correct greeting for '${name}'`, async () => {
     const request = Substitute.for<HttpRequest>();
-    (request.query.returns as any)({
-      name: "Jan Aagaard"
-    });
+    (request.query.returns as any)({ name });
+    (request.body.returns as any)({});
 
     const context = Substitute.for<Context>();
-    (context.req as any).returns({
-      req: request
-    });
+    (context.req as any).returns({ req: request });
 
     const response = await greet(context, request);
 
-    expect(response.body).toBe("Hello Jan Aagaard.");
+    expect(response.body).toBe(greeting);
   });
 });
