f72c1f17c37930fa91a86fd3ff1494a7d838b1fb
Jan Aagaard <jan@aagaard.net>
2019-07-22 21:33:48 +0200

    Shorten comment

--- .circleci/publish-to-azure.sh
+++ .circleci/publish-to-azure.sh
@@ -13,7 +13,7 @@ cd $script_folder/../dist
 
 func_command="$script_folder/../node_modules/.bin/func"
 resource_group_name="azure-functions-typescript-$1"
-# There is probably a simpler way to extract functions_name from the returned JSON, but this works.
+# There is probably a simpler way to extract functions_name from the returned JSON.
 functions_name=`az resource list -g $resource_group_name | grep -o -E '"name": "aft-.*-functions"' | grep -o -E "aft-.*-functions"`
 echo "Publishing to $functions_name."
 $func_command azure functionapp publish $functions_name
