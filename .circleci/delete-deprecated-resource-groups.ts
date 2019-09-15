43ec203aaf6dda3c0aefe3479c2ee0113c1a2f7c
Jan Aagaard <jan@aagaard.net>
2019-09-15 22:19:07 +0200

    Parse results

--- .circleci/delete-deprecated-resource-groups.ts
+++ .circleci/delete-deprecated-resource-groups.ts
@@ -3,12 +3,32 @@ const exec = util.promisify(require("child_process").exec);
 
 async function executeCommand(command: string): Promise<string> {
   const result = await exec(command);
-  return result.stdout;
+  return result.stdout.trim();
+}
+
+async function getRemoteBranchNames(): Promise<Array<string>> {
+  const prefixLength = "f47884521bf37f59bf0de7298989215ea4670311\trefs/heads/"
+    .length;
+  const rawResponse = await executeCommand("git ls-remote --heads --refs");
+  const fullArray = rawResponse.split("\n");
+  const branchNames = fullArray.map(rawName => rawName.substring(prefixLength));
+  return branchNames;
+}
+
+interface ResourceGroup {
+  name: string;
+}
+
+async function getGroupNames(): Promise<Array<string>> {
+  const rawGroups = await executeCommand("az group list");
+  const jsonGroups = JSON.parse(rawGroups) as Array<ResourceGroup>;
+  const groupNames = jsonGroups.map(groupInfo => groupInfo.name);
+  return groupNames;
 }
 
 async function run() {
-  console.info(await executeCommand("git ls-remote --heads --refs"));
-  console.info(await executeCommand("az group list"));
+  console.info(JSON.stringify(await getRemoteBranchNames(), undefined, 2));
+  console.info(JSON.stringify(await getGroupNames(), undefined, 2));
 }
 
 run();
