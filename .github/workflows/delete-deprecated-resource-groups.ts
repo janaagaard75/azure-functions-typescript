a1665aebb365ff58157e72d40ebae7222a4eb540
Jan Aagaard <jan.aagaard@pronestor.com>
2021-11-17 07:46:39 +0100

    Format

--- .github/workflows/delete-deprecated-resource-groups.ts
+++ .github/workflows/delete-deprecated-resource-groups.ts
@@ -13,7 +13,9 @@ async function getBranchNames(): Promise<Array<string>> {
     .length;
   const rawResponse = await executeCommand("git ls-remote --heads --refs");
   const fullArray = rawResponse.split("\n");
-  const branchNames = fullArray.map(rawName => rawName.substring(prefixLength));
+  const branchNames = fullArray.map((rawName) =>
+    rawName.substring(prefixLength)
+  );
   return branchNames;
 }
 
@@ -28,21 +30,21 @@ interface ResourceGroup {
 async function getGroupNames(): Promise<Array<string>> {
   const rawGroups = await executeCommand("az group list");
   const jsonGroups = JSON.parse(rawGroups) as Array<ResourceGroup>;
-  const groupNames = jsonGroups.map(groupInfo => groupInfo.name);
+  const groupNames = jsonGroups.map((groupInfo) => groupInfo.name);
   return groupNames;
 }
 
 async function run() {
-  const expectedGroups = (await getBranchNames()).map(branchName =>
+  const expectedGroups = (await getBranchNames()).map((branchName) =>
     toResourceGroupName(branchName)
   );
   const actualGroups = await getGroupNames();
   const groupsToDelete = actualGroups.filter(
-    group => !expectedGroups.includes(group) && group !== productionGroup
+    (group) => !expectedGroups.includes(group) && group !== productionGroup
   );
 
   if (groupsToDelete.length >= 1) {
-    groupsToDelete.forEach(async group => {
+    groupsToDelete.forEach(async (group) => {
       console.info(`Deleting deprecated resource group ${group}.`);
       await executeCommand(`az group delete --name ${group} --yes`);
     });
