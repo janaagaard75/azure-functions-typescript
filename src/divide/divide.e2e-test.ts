f2e08ab4985796dd55c5b293290b926b64027fff
Jan Aagaard <jan@aagaard.net>
2021-11-18 22:02:10 +0100

    Specify that is has to be integers

--- src/divide/divide.e2e-test.ts
+++ src/divide/divide.e2e-test.ts
@@ -4,11 +4,11 @@ import { TestHelper } from "../TestHelper";
 describe("divide endpoint", () => {
   test("returns correct fraction supplying a dividend and a devisor.", async () => {
     const apiRootUrl = await TestHelper.getApiRootUrl();
-    const response = await fetch(`${apiRootUrl}/divide/5.5/2.5`);
+    const response = await fetch(`${apiRootUrl}/divide/5/2`);
     expect(response.ok).toBe(true);
 
     const responseBody = await response.text();
-    expect(responseBody).toBe("5.5/2.5 is 2.2.");
+    expect(responseBody).toBe("5/2 is 2.5.");
   });
 
   test("returns 400 Bad Request if trying to divide by zero.", async () => {
